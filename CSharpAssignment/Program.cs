a51d158500846014e4a41a10b389d8298b6c6c7a
omkarj <omkarj@MAVCHN0522201>
2022-12-23 17:59:08 +0530

    Add project files.

---
+++ CSharpAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Math.VolumeFormulae;
+
+namespace Math.Formulae
+{
+    public  class Program
+    {
+        public static void Main(String[] args)
+        {
+           double result= Volume.VolumnOfSphere(12);
+            Console.WriteLine(result);
+            Volume obj = new Volume();
+            double result1 =obj.VolumnOfCuboid(10,10,10);
+          
+
+             result1 = obj.VolumnOfCuboid(20, 20, 20);
+
+             result1 = obj.VolumnOfCuboid(30, 30, 30);
+            Console.WriteLine(result1);
+            Volume.GetAuthorName();
+            obj.Quit();       }  
+    }
+}
---
+++ CSharpAssignment/Volume.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace Math.VolumeFormulae
+{
+    public class Volume
+    {
+       
+
+          public  static double VolumnOfSphere(double radius)
+            {
+                double volSphere = 4 * 3.14 * radius * radius * radius / 3;
+                return volSphere;
+            }
+        public   double VolumnOfCuboid(double length, double width, double height)
+        {
+            double volcuboid = length * width * height;
+            return volcuboid;
+        }
+        
+        public  double VolumeOfCylinder(double radius,double height)
+        {
+            return 3.1 * radius * radius * height;
+
+        }
+        public static String GetAuthorName()
+        {
+            return "Omkar Jadhav";
+        }
+
+        public  void Quit()
+        { 
+        
+        }
+       
+
+
+    }
+}
