76cee3615a8fdf7297290fa0f5dffb1adfd07eb6
Omkar Bhausaheb Jadhav <omkarjadhav4148@gmail.com>
2022-12-26 16:24:10 +0530

    Emp

---
+++ EmployeeProject/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeProject
+{
+    public class Employee
+    {
+        public int empId;
+        public string empName;
+        public double empSalary;
+        public static string companyName = "Maveric";
+        public static string companLocation = "Pune";
+    }
+}
