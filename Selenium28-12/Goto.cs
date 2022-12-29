713430e66c1ffba06ca6516834a1bf5713fd9279
Omkar Bhausaheb Jadhav <omkarjadhav4148@gmail.com>
2022-12-29 14:33:48 +0530

    Syncronization

---
+++ Selenium28-12/Goto.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+using System.Xml.Linq;
+
+namespace Selenium28_12
+{
+    public class Goto
+    {
+        public static void Main(string[] args)
+        {
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Url = "https://www.goto.com/meeting/";
+            // Console.WriteLine(driver.PageSource);
+           
+            driver.FindElement(By.XPath("//button[text()='Accept Recommended Settings']")).Click();
+            Thread.Sleep(4000);
+            IWebElement element = driver.FindElement(By.XPath("//a[text()='Try Free']"));
+            Actions actions = new Actions(driver);
+            actions.MoveToElement(element).Click().Build().Perform();
+          
+            driver.FindElement(By.XPath("//label[text()='First Name']")).SendKeys("");
+            driver.FindElement(By.XPath("//label[text()='Last Name']")).SendKeys("");
+            driver.FindElement(By.XPath("//label[text()='Business Email']")).SendKeys("");
+            driver.FindElement(By.XPath("//label[text()='Phone Number (Optional)']")).SendKeys("");
+            SelectElement dropdown = new SelectElement (driver.FindElement(By.XPath("//select[@class='select__select  ']")));
+            dropdown.SelectByText("10 - 99");
+
+
+
+        }
+    }
+}
---
+++ Selenium28-12/MediBuddy.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium28_12
+{
+    public  class MediBuddy
+    {
+        public static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Url = "https://www.medibuddy.in/";
+            driver.FindElement(By.XPath("//a[text()='Login']")).Click();
+            driver.FindElement(By.XPath("//div[@class ='coperate']")).Click();
+            driver.FindElement(By.XPath("//div[@class ='coperate']")).Click();
+            driver.FindElement(By.Name("userName")).SendKeys("john");
+            driver.FindElement(By.XPath("//button[text() ='Proceed']")).Click();
+            driver.FindElement(By.Name("password")).SendKeys("john123");
+            driver.FindElement(By.XPath("//span[text() ='Show password']")).Click();
+            driver.FindElement(By.XPath("//button[text() ='Log in']")).Click(); 
+
+
+
+
+        }
+    }
+}
